9f8967e91b7e9ca0279719cb0040c8b5f27e905b
Navid Kalaei <navidkalaei@gmail.com>
2021-01-23 00:24:54 +0330

    chore: redirect main pages to kvm in case project name is not provided

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   webpack: (config, _) => {
     return config
   },
+  redirects: async () => ([
+    {
+      source: '/maps',
+      destination: '/maps/kvm',
+      permanent: true
+    },
+    {
+      source: '/tables',
+      destination: '/tables/kvm',
+      permanent: true
+    }
+  ])
 }
