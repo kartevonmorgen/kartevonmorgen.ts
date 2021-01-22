a5fbe8ebcb3b393a3be2e5ec4082993087146080
Navid Kalaei <navidkalaei@gmail.com>
2021-01-23 00:13:43 +0330

    refactor: remove the file loader plugin in webpack

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   webpack: (config, _) => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      loader: "file-loader",
-      options: {
-        outputPath: '../public/assets/', // if you don't use ../ it will put it inside ".next" folder by default
-        publicPath: 'assets/',
-      }
-    });
-
     return config
   },
 }
