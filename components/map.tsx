7582f27bb3362513b29bfb27d97969df43cf812f
Navid Kalaei <navidkalaei@gmail.com>
2021-01-23 00:11:33 +0330

    refactor: fetch the images based on the static serve policy instead of import

--- components/map.tsx
+++ components/map.tsx
@@ -2,21 +2,23 @@ import {Icon} from 'leaflet'
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
-import initiativeImg from '../public/customizations/kvm/icons/initiative-plain.png'
-import initiativeImg2x from '../public/customizations/kvm/icons/initiative-plain-2x.png'
 
 
 const icon = new Icon({
-  iconUrl: initiativeImg,
-  iconRetinaUrl: initiativeImg2x,
-
+  iconUrl: '/customizations/kvm/icons/initiative-plain.png',
+  iconRetinaUrl: '/customizations/kvm/icons/initiative-plain-2x.png',
 });
 
 
 const Map = () => {
   return (
     <div id="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{height: '100%', width: '100%'}}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{height: '100%', width: '100%'}}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
