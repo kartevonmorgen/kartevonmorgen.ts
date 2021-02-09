ba928c8b92f884abc98420a1d305afae70059e71
Navid Kalaei <navidkalaei@gmail.com>
2021-02-09 23:57:07 +0330

    feat: add interface for map location props

--- components/map.tsx
+++ components/map.tsx
@@ -1,40 +1,51 @@
-import {Icon} from 'leaflet'
-import {MapContainer, Marker, Popup, TileLayer, ZoomControl} from 'react-leaflet'
+import { FC } from 'react'
+import { Icon } from 'leaflet'
+import { MapContainer, Marker, Popup, TileLayer, ZoomControl } from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
 
 const icon = new Icon({
-  iconUrl: '/projects/kvm/icons/initiative-plain.png',
-  iconRetinaUrl: '/projects/kvm/icons/initiative-plain-2x.png',
-});
+  iconUrl: '/projects/main/icons/initiative-plain.png',
+  iconRetinaUrl: '/projects/main/icons/initiative-plain-2x.png',
+})
 
 
-const Map = () => {
+export interface MapLocationProps {
+  lat: number
+  lng: number
+  zoom: number
+}
+
+interface MapProps extends MapLocationProps {
+
+}
+
+const Map: FC = () => {
   return (
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={true}
-        style={{height: '100%', width: '100%'}}
-        zoomControl={false}
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: '100%', width: '100%' }}
+      zoomControl={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <ZoomControl position="bottomright"/>
+      <Marker
+        position={[51.505, -0.09]}
+        icon={icon}
       >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <ZoomControl position="bottomright"/>
-        <Marker
-          position={[51.505, -0.09]}
-          icon={icon}
-        >
-          <Popup>
-            A pretty CSS3 popup.
-            <br/>
-            Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+        <Popup>
+          A pretty CSS3 popup.
+          <br/>
+          Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
