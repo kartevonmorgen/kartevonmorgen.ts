044fe74e0a300ed983624ee9f4efefdd7f9fd50f
Navid Kalaei <navidkalaei@gmail.com>
2021-02-18 15:57:22 +0330

    feat: connect type chooser to router

--- components/TypeChooser.tsx
+++ components/TypeChooser.tsx
@@ -1,19 +1,21 @@
-import { Component } from 'react'
+import { FC } from 'react'
 
 import { Tag } from 'antd'
 import Category from '../dtos/Categories'
 
-import {lime, green, magenta} from '@ant-design/colors'
+import { green, lime, magenta } from '@ant-design/colors'
+import { NextRouter, useRouter } from 'next/router'
+import { convertQueryParamToArray, updateRoutingQuery } from '../utils/utils'
 
 const { CheckableTag } = Tag
 
-interface Types {
+interface Type {
   id: Category
   name: string
   color: string
 }
 
-const types: Types[] = [
+const types: Type[] = [
   {
     id: Category.INITIATIVE,
     name: 'Initiative',
@@ -31,39 +33,70 @@ const types: Types[] = [
   },
 ]
 
-class TypeChooser extends Component {
-  state = {
-    selectedTags: [],
+
+const handleChange = (
+  typeId: Category,
+  checked: boolean,
+  selectedTypes: Category[],
+  router: NextRouter,
+) => {
+
+  const {query} = router
+
+  let nextSelectedTypes = [] as Category[]
+  if (selectedTypes.length === types.length) {
+    // if all are selected -> disable others
+    nextSelectedTypes = [typeId]
+  } else if(selectedTypes.length === 1 && selectedTypes[0] === typeId) {
+    // if this type is the only active type -> select all types to prevent non-selection
+    nextSelectedTypes = types.map(type => type.id)
+  } else {
+    // everything is normal
+    if (checked) {
+      nextSelectedTypes = [...selectedTypes, typeId]
+    } else {
+      nextSelectedTypes = selectedTypes.filter(tId => tId !== typeId)
+    }
   }
 
-  handleChange(tag, checked) {
-    const { selectedTags } = this.state
-    const nextSelectedTags = checked ? [...selectedTags, tag] : selectedTags.filter(t => t !== tag)
-    this.setState({ selectedTags: nextSelectedTags })
-  }
+  const newQueryParams = updateRoutingQuery(query, {type: nextSelectedTypes})
 
-  render() {
-    const { selectedTags } = this.state
-    return (
-      <>
-        {types.map(type => {
-          const isChecked = selectedTags.indexOf(type.id) > -1
-          return (
-            <CheckableTag
-              key={type.id}
-              checked={isChecked}
-              onChange={checked => this.handleChange(type.id, checked)}
-              style={{
-                backgroundColor: isChecked && type.color,
-              }}
-            >
-              {type.name}
-            </CheckableTag>
-          )
-        })}
-      </>
-    )
-  }
+  router.replace(
+    {
+      pathname: '/maps/[project]',
+      query: newQueryParams,
+    },
+    undefined,
+    { shallow: true }
+  )
+}
+
+
+const TypeChooser: FC = () => {
+  const router = useRouter()
+  const { query } = router
+  const { type: queryTypes } = query
+  const selectedTypes = convertQueryParamToArray(queryTypes) as Category[]
+
+  return (
+    <>
+      {types.map(type => {
+        const isChecked = selectedTypes.indexOf(type.id) > -1
+        return (
+          <CheckableTag
+            key={type.id}
+            checked={isChecked}
+            onChange={checked => handleChange(type.id, checked, selectedTypes, router)}
+            style={{
+              backgroundColor: isChecked && type.color,
+            }}
+          >
+            {type.name}
+          </CheckableTag>
+        )
+      })}
+    </>
+  )
 }
 
 
