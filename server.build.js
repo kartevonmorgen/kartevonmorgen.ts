ecf3f42119edb28adf638206e971c1c7a97be96a
Navid Kalaei <navidkalaei@gmail.com>
2021-03-21 18:11:59 +0330

    feat: add Dockerfile

--- server.build.js
+++ server.build.js
@@ -2,6 +2,6 @@ const cli = require('next/dist/cli/next-build')
 
 
 process.env.HOSTNAME = process.env.HOSTNAME || '0.0.0.0'
-process.env.PORT = process.env.PORT || '8000'
+process.env.PORT = process.env.PORT || '3000'
 
 cli.nextBuild()
--- server.dev.js
+++ server.dev.js
@@ -2,7 +2,7 @@ const cli = require('next/dist/cli/next-dev')
 
 
 process.env.HOSTNAME = process.env.HOSTNAME || '0.0.0.0'
-process.env.PORT = process.env.PORT || '8000'
+process.env.PORT = process.env.PORT || '3000'
 
 
 cli.nextDev(
