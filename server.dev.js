de136be64003a12b7444f143b022ef8c0080d167
Navid Kalaei <navidkalaei@gmail.com>
2021-06-24 18:04:35 +0430

    feat: add python scripts to cache popular tags

--- server.dev.js
+++ server.dev.js
@@ -5,7 +5,6 @@ dotenv.config()
 
 process.env.HOSTNAME = process.env.HOSTNAME || '0.0.0.0'
 process.env.PORT = process.env.PORT || '3000'
-process.env.NEXT_PUBLIC_BASICS_API = 'https://api.ofdb.io/v0'
 process.env.NEXT_PUBLIC_SELF_API = `http://${process.env.HOSTNAME}:${process.env.PORT}/api/v0`
 
 
