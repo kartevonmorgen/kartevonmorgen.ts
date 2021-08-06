d583ba8b2f79a2cf5ef6eac189c66a5eac7b536e
Ivan Filipchik <wot.vania@mail.ru>
2021-08-06 12:25:03 +0300

    feat: translation for button text

--- components/ShowMapBtn.tsx
+++ components/ShowMapBtn.tsx
@@ -1,8 +1,14 @@
 import { Button } from 'antd'
 import React from 'react'
+import useTranslation from 'next-translate/useTranslation'
 
-export const ShowMapBtn = React.memo((props: any) => {
-    return <Button size={'large'} onClick={props.showMapOnBtnClick}>
-      Show map...
-    </Button>
+type ShowMapBtnPropsType = {
+  showMapOnBtnClick: () => void
+}
+
+export const ShowMapBtn = React.memo((props: ShowMapBtnPropsType) => {
+  const {t} = useTranslation('home')
+  return <Button size={'large'} onClick={props.showMapOnBtnClick}>
+    {t('landingPage.city-search.show-map')}
+  </Button>
 })
