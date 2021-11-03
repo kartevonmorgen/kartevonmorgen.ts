6aa3ff07a81b82a9ca65c59c3fd8ee5633399590
Navid Kalaei <navidkalaei@gmail.com>
2021-11-03 12:55:54 +0330

    fix: set the self dev domain of front end to http://dev.kartevonmorgen.org

--- server.build.dev.js
+++ server.build.dev.js
@@ -4,7 +4,7 @@ const cli = require('next/dist/cli/next-build')
 dotenv.config()
 
 process.env.NEXT_PUBLIC_BASICS_API = 'http://dev.ofdb.io/v0'
-process.env.NEXT_PUBLIC_SELF_DOMAIN = `https://ssr.kartevonmorgen.org`
+process.env.NEXT_PUBLIC_SELF_DOMAIN = `https://dev.kartevonmorgen.org`
 process.env.NEXT_PUBLIC_SELF_API = `${process.env.NEXT_PUBLIC_SELF_DOMAIN}/api/v0`
 process.env.DB_NAME = 'kartevonmorgen.development.sqlite'
 
