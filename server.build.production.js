2809fa17231fbe40ebf95892826909b850a83980
Navid Kalaei <navidkalaei@gmail.com>
2021-11-08 18:06:53 +0330

    fix: change the production ofdb domain from openfairdb.org to kartevonmorgen.org

--- server.build.production.js
+++ server.build.production.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 process.env.NEXT_PUBLIC_SELF_DOMAIN = `https://ssr.kartevonmorgen.org`
 process.env.NEXT_PUBLIC_SELF_API = `${process.env.NEXT_PUBLIC_SELF_DOMAIN}/api/v0`
-process.env.NEXT_PUBLIC_BASICS_API = 'https://openfairdb.org/api/v0'
+process.env.NEXT_PUBLIC_BASICS_API = 'https://kartevonmorgen.org/api/v0'
 process.env.DB_NAME = 'kartevonmorgen.sqlite'
 
 
