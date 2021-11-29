5ac92dcd0335a3a699a744ad097ff85eeb949eb8
Navid Kalaei <navidkalaei@gmail.com>
2021-11-29 20:19:15 +0330

    fix: add fallback for api/v0/maps/[project]/config/burger-menu

--- pages/api/v0/maps/[project]/config/burger-menu.ts
+++ pages/api/v0/maps/[project]/config/burger-menu.ts
@@ -3,6 +3,10 @@ import path from 'path'
 import { LinkWithIcon } from '../../../../../../dtos/LinkWithIcon'
 
 
+const getPath = (project: string = 'main'): string => {
+  return `./public/projects/${project}/burger-menu.json`
+}
+
 export default async (req, res) => {
   const {
     query: { project },
@@ -17,12 +21,20 @@ export default async (req, res) => {
     return
   }
 
-  const fileContent = fs.readFileSync(
-    path.resolve(`./public/projects/${project}/burger-menu.json`),
-    'utf8',
-  )
-
+  let fileContent: string = ''
+  try {
+    fileContent = fs.readFileSync(
+      path.resolve(getPath(project as string)),
+      'utf8',
+    )
+  } catch (e) {
+    fileContent = fs.readFileSync(
+      path.resolve(getPath()),
+      'utf8',
+    )
+  }
   const burgerMenuItems: LinkWithIcon[] = JSON.parse(fileContent.toString())
+
   res
     .status(200)
     .json(burgerMenuItems)
