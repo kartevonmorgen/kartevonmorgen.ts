ff61971e9c4deedf475799851ff06990f1e960bd
dmitrap <dmitrap@gmail.com>
2021-12-22 15:05:00 +0500

    Merge branch 'develop' into project/co-map.ru/integrated

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -2,52 +2,12 @@ import { FC } from 'react'
 import DynamicHomePage from '../dynamic-components/HomePage'
 
 
-const { Content } = Layout
+const Home: FC = () => {
 
-const Home: FC = () => (
-  <div>
-    <HomeEntityRedirector/>
-
-    <Layout>
-
-      <HomeHeader/>
-
-      <Banner/>
-
-      <Content
-        style={{
-          backgroundColor: 'white',
-          padding: 60,
-          paddingTop: 24,
-        }}
-      >
-        <HomeContent/>
-
-        <HomeDivider/>
-
-        <Tutorial/>
-
-        <Vision/>
-
-        <HomeDivider/>
-
-        <Team/>
-
-        <HomeDivider/>
-
-        <Creators/>
-
-        <HomeDivider/>
-
-        <Partners/>
-
-      </Content>
-
-      <Footer/>
-
-    </Layout>
-  </div>
-)
+  return (
+    <DynamicHomePage/>
+  )
+}
 
 
 export default Home
