529e1c3a0ef70f4f3396d0448c624e0f3fad746b
Navid Kalaei <navidkalaei@gmail.com>
2022-01-12 09:29:06 +0330

    devop: add tag synchronizer

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  apps: [{
-    name: 'kartevonmorgen-dev',
-    script: 'yarn start:dev',
-  }],
+  apps: [
+    {
+      name: 'kartevonmorgen-dev',
+      script: 'yarn start:dev',
+    },
+    {
+      name: 'kartevonmorgen-tag-synchronizer-dev',
+      script: 'python3 ./scripts/python/tag-frequency-cron.py --dev --fetch-all-on-start --sync-once --log-level debug',
+    },
+  ],
 }
