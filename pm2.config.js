43b8b72ea8f863533ec6ec12d80e1c28d527c8e8
Navid Kalaei <navidkalaei@gmail.com>
2022-01-13 21:56:47 +0330

    devop: break down running scripts for tag synchronizer

--- pm2.config.js
+++ pm2.config.js
@@ -6,7 +6,9 @@ module.exports = {
     },
     {
       name: 'kartevonmorgen-tag-synchronizer-dev',
-      script: 'python3 scripts/python/tag-frequency-cron.py --dev --kvm-path . --fetch-all-on-start --interval-for-all-tags 300 --interval-for-least-used-tags 60 --log-level debug',
+      interpreter: 'python3',
+      script: 'scripts/python/tag-frequency-cron.py',
+      args: '--dev --kvm-path . --fetch-all-on-start --interval-for-all-tags 300 --interval-for-least-used-tags 60 --log-level debug',
     },
   ],
 }
