cd5e94dab3ac6859627e1003618bf10834a2e3b1
dmitrap <dmitrap@gmail.com>
2022-02-22 19:41:42 +0500

    make a little changes in translation and set default coordinates and zoom to Moscow

--- components/TypeTag.tsx
+++ components/TypeTag.tsx
@@ -1,18 +1,16 @@
 import { FC } from 'react'
 import { Tag, TagProps } from 'antd'
-
+import useTranslation from 'next-translate/useTranslation'
 
 interface TypeTagProps extends TagProps {
   type: string
 }
 
 const TypeTag: FC<TypeTagProps> = ({ type, ...tagProps }) => {
+  const { t } = useTranslation('map')
   return (
-    <Tag
-      {...tagProps}
-      className={`${type}-tag`}
-    >
-      {type}
+    <Tag {...tagProps} className={`${type}-tag`}>
+      {t(`category.${type}`) || type}
     </Tag>
   )
 }
