aa48b188ccb8afaf37074907a4b5109370e04def
Navid Kalaei <navidkalaei@gmail.com>
2023-05-21 00:00:10 +0330

    feat(#176): use correct fields for address-formatter

--- components/EntityAddress.tsx
+++ components/EntityAddress.tsx
@@ -10,7 +10,15 @@ interface EntryAddressProps extends EntryAddressDTO {
 }
 
 const EntityAddress: FC<EntryAddressProps> = (props) => {
-  const addressFields = pickBy(props, isString)
+  const { city, country, state, street, zip } = props
+  const addressFields = pickBy({
+    city,
+    country,
+    state,
+    road: street,
+    postcode: zip
+  }, isString)
+
 
   return (
     <div
@@ -22,10 +30,10 @@ const EntityAddress: FC<EntryAddressProps> = (props) => {
           marginRight: 4,
         }}
       >
-        <FontAwesomeIcon icon="map-marker-alt"/>
+        <FontAwesomeIcon icon='map-marker-alt' />
       </div>
 
-      {addressFormatter.format(addressFields, {countryCode: 'DE'})}
+      {addressFormatter.format(addressFields, { countryCode: 'DE' })}
     </div>
   )
 }
