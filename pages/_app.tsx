7d992002e033f8d30132f0827a627af8955eb890
Navid Kalaei <navidkalaei@gmail.com>
2023-07-02 19:52:46 +0330

    feat(#104): change the font to Rubik

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { ConfigProvider } from 'antd'
 import store from '../store'
 import '../utils/icons/font-awesome'
 import Layout from '../components/Layout'
 import GlobalLocale from '../components/GlobalLocale'
 import '../styles/globals.css'
+import 'typeface-rubik/index.css'
 
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
@@ -13,8 +15,16 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
     <GlobalLocale/>
 
     <Provider store={store}>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: '"Rubik", sans-serif',
+          }
+        }}
+      >
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ConfigProvider>
     </Provider>
   </Layout>
 )
