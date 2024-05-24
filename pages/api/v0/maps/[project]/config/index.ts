5e7ac55bfcb72ceaa109a5ef66f3cdb39570acd7
Navid Kalaei <navidkalaei@gmail.com>
2024-05-24 05:00:10 +0330

    feat: read map config file directly from server

--- pages/api/v0/maps/[project]/config/index.ts
+++ pages/api/v0/maps/[project]/config/index.ts
@@ -3,11 +3,64 @@ import path from 'path'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import MapPageConfigs from '../../../../../../dtos/MapPageConfigs'
+import { MapColorModes } from '../../../../../../components/MapColorStyle'
 
 
 const getPath = (project: string = 'main'): string => {
   return `./public/projects/${project}/config.json`
 }
+
+
+export const parseConfigFile = (project: string = 'main'): MapPageConfigs => {
+  let mapPageConfigs: MapPageConfigs = {
+    "map": {
+      "location": {
+        "lat": 50.8129,
+        "lng": 5.6030,
+        "zoom": 6
+      },
+      "colorStyle": MapColorModes.GRAY
+    },
+    "popularTags": {
+      "min_count": 2
+    },
+    "sidebar": {
+      "title": "Kartevonmorgen"
+    }
+  }
+
+  let fileContent: string = ''
+  try {
+    fileContent = fs.readFileSync(
+      path.resolve(getPath(project as string)),
+      'utf8',
+    )
+  } catch (e) {
+    console.error('api map config: failed to read config file for project: ', project)
+    console.error(e)
+    try {
+      console.log('api map config: trying to read default config file')
+      fileContent = fs.readFileSync(
+        path.resolve(getPath()),
+      'utf8',
+      )
+    } catch (e) {
+      console.error('api map config: failed to read default config file')
+      console.error(e)
+    }
+  }
+
+  try {
+    mapPageConfigs = JSON.parse(fileContent.toString())
+  } catch(e) {
+    console.error('api map config: failed to parse config file')
+    console.error(e)
+  }
+  
+  console.log(mapPageConfigs)
+  return mapPageConfigs
+}
+
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -23,22 +76,8 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     return
   }
 
-  // todo: eighter move it to env or consts
-  // todo: catch the error if the file is not found
-
-  let fileContent: string = ''
-  try {
-    fileContent = fs.readFileSync(
-      path.resolve(getPath(project as string)),
-      'utf8',
-    )
-  } catch (e) {
-    fileContent = fs.readFileSync(
-      path.resolve(getPath()),
-      'utf8',
-    )
-  }
-  const mapPageConfigs: MapPageConfigs = JSON.parse(fileContent.toString())
+  
+  const mapPageConfigs: MapPageConfigs = parseConfigFile(project as string)
 
   res
     .status(200)
