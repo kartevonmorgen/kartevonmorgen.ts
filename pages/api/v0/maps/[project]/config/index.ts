406d5a17da76069995002dc63cd7517a11351965
Navid Kalaei <navidkalaei@gmail.com>
2024-07-09 16:10:59 +0330

    fix: handle configs with a wrong schema

--- pages/api/v0/maps/[project]/config/index.ts
+++ pages/api/v0/maps/[project]/config/index.ts
@@ -5,61 +5,59 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import MapPageConfigs, { MapPageConfigsSchema } from '../../../../../../dtos/MapPageConfigs'
 import { MapColorModes } from '../../../../../../components/MapColorStyle'
 
-
 const getPath = (project: string = 'main'): string => {
   return `./public/projects/${project}/config.json`
 }
 
-
-export const parseConfigFile = (project: string = 'main'): MapPageConfigs => {
+export const getConfigFile = (project: string = 'main'): MapPageConfigs => {
   let mapPageConfigs: MapPageConfigs = {
-    "map": {
-      "location": {
-        "lat": 50.8129,
-        "lng": 5.6030,
-        "zoom": 6
+    map: {
+      location: {
+        lat: 50.8129,
+        lng: 5.603,
+        zoom: 6,
       },
-      "colorStyle": MapColorModes.GRAY
+      colorStyle: MapColorModes.GRAY,
     },
-    "popularTags": {
-      "min_count": 2
+    popularTags: {
+      min_count: 2,
     },
-    "sidebar": {
-      "title": "Kartevonmorgen"
-    }
+    sidebar: {
+      title: '',
+    },
   }
 
   let fileContent: string = ''
   try {
-    fileContent = fs.readFileSync(
-      path.resolve(getPath(project as string)),
-      'utf8',
-    )
+    fileContent = fs.readFileSync(path.resolve(getPath(project as string)), 'utf8')
   } catch (e) {
     console.error('api map config: failed to read config file for project: ', project)
-    console.error(e)
+    // console.error(e)
     try {
-      console.log('api map config: trying to read default config file')
-      fileContent = fs.readFileSync(
-        path.resolve(getPath()),
-      'utf8',
-      )
+      console.log('api map config: trying to read default config file for project: ', project)
+      fileContent = fs.readFileSync(path.resolve(getPath()), 'utf8')
     } catch (e) {
       console.error('api map config: failed to read default config file for project: ', project)
-      console.error(e)
+      // console.error(e)
     }
   }
 
   try {
     mapPageConfigs = MapPageConfigsSchema.parse(JSON.parse(fileContent.toString()))
-  } catch(e) {
+  } catch (e) {
     console.error('api map config: failed to parse config file for project: ', project)
-    console.error(e)
+    try {
+      console.log('api map config: trying to read default config file for project: ', project)
+      fileContent = fs.readFileSync(path.resolve(getPath()), 'utf8')
+    } catch (e) {
+      console.error('api map config: failed to read default config file for project: ', project)
+      // console.error(e)
+    }
+    // console.error(e)
   }
-  
+
   return mapPageConfigs
 }
-
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -75,10 +73,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     return
   }
 
-  
-  const mapPageConfigs: MapPageConfigs = parseConfigFile(project as string)
+  const mapPageConfigs: MapPageConfigs = getConfigFile(project as string)
 
-  res
-    .status(200)
-    .json(mapPageConfigs)
+  res.status(200).json(mapPageConfigs)
 }
