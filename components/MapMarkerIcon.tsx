72d595103fbb36618b7a52f6077a74f8da12e402
Helmut Wolman <Helmut@kartevonmorgen.org>
2025-07-26 22:05:12 +0200

    Update MapMarkerIcon.tsx
    I changed my commit from the first to the second format:
    
    1. Use import { IoFootball } from "react-icons/io5"; if you are using the regular react-icons package.
    2. Use import { IoFootball } from '@react-icons/io5/IoFootball' if you are using the @react-icons/all-files package for optimized imports.
    
    You cannot mix these two styles interchangeably unless you have both packages installed and configured accordingly. The import paths and the package structure are different. Always check which package you are using in your package.json and follow its documentation.

--- components/MapMarkerIcon.tsx
+++ components/MapMarkerIcon.tsx
@@ -4,18 +4,18 @@ import { FaFutbol } from '@react-icons/all-files/fa/FaFutbol'
 import { FaLock } from '@react-icons/all-files/fa/FaLock'
 import { IoMdFootball } from '@react-icons/all-files/io/IoMdFootball'
 import { IoIosFootball } from '@react-icons/all-files/io/IoIosFootball'
-import { IoFootball } from @react-icons/io5/IoFootball'
-import { IoFootballOutline } from @react-icons/io5/IoFootballOutline'
+import { IoFootball } from '@react-icons/io5/IoFootball'
+import { IoFootballOutline } from '@react-icons/io5/IoFootballOutline'
 // Teikei Icons: 
-import { TbMapPinDown } from "react-icons/tb";
-import { IoStorefrontSharp } from "react-icons/io5";
-import { PiPlant } from "react-icons/pi";
-import { PiFarm } from "react-icons/pi";
-import { FaTruckLoading } from "react-icons/fa";
-import { FaTruckArrowRight } from "react-icons/fa6";
-import { TbDropletStar } from "react-icons/tb";
-import { RiHomeOfficeFill } from "react-icons/ri";
-import { FaLaptopCode } from "react-icons/fa";
+import { TbMapPinDown } from '@react-icons/all-files/tb/TbMapPinDown'
+import { IoStorefrontSharp } from '@react-icons/all-files/io5/IoStorefrontSharp'
+import { PiPlant } from '@react-icons/all-files/pi/PiPlant'
+import { PiFarm } from '@react-icons/all-files/pi/PiFarm'
+import { FaTruckLoading } from '@react-icons/all-files/fa/FaTruckLoading'
+import { FaTruckArrowRight } from '@react-icons/all-files/fa6/FaTruckArrowRight'
+import { TbDropletStar } from '@react-icons/all-files/tb/TbDropletStar'
+import { RiHomeOfficeFill } from '@react-icons/all-files/ri/RiHomeOfficeFill'
+import { FaLaptopCode } from '@react-icons/all-files/fa/FaLaptopCode'
 // others?
 import { IconBaseProps, IconType } from 'react-icons'
 interface MapMarkerIconProps extends IconBaseProps {
@@ -44,6 +44,9 @@ const MapMarkerIcon: FC<MapMarkerIconProps> = (props) => {
       break
     case 'IoMdFootball':
       Icon = IoMdFootball
+      break
+    case 'IoFootballOutline':
+      Icon = IoFootballOutline
       break
       // Teikei Icons: 
     case 'TbMapPinDown':
