7f51f64733bc55aa84e3266e5cf39a527305bbb0
Navid Kalaei <navidkalaei@gmail.com>
2025-08-04 14:26:35 -0700

    fix: import the newly pin icons properly

--- components/MapMarkerIcon.tsx
+++ components/MapMarkerIcon.tsx
@@ -1,21 +1,14 @@
 import { FC } from 'react'
 
-import { FaFutbol } from '@react-icons/all-files/fa/FaFutbol'
-import { FaLock } from '@react-icons/all-files/fa/FaLock'
-import { IoMdFootball } from '@react-icons/all-files/io/IoMdFootball'
-import { IoIosFootball } from '@react-icons/all-files/io/IoIosFootball'
-import { IoFootball } from '@react-icons/io5/IoFootball'
-import { IoFootballOutline } from '@react-icons/io5/IoFootballOutline'
-// Teikei Icons: 
-import { TbMapPinDown } from '@react-icons/all-files/tb/TbMapPinDown'
-import { IoStorefrontSharp } from '@react-icons/all-files/io5/IoStorefrontSharp'
-import { PiPlant } from '@react-icons/all-files/pi/PiPlant'
-import { PiFarm } from '@react-icons/all-files/pi/PiFarm'
-import { FaTruckLoading } from '@react-icons/all-files/fa/FaTruckLoading'
-import { FaTruckArrowRight } from '@react-icons/all-files/fa6/FaTruckArrowRight'
-import { TbDropletStar } from '@react-icons/all-files/tb/TbDropletStar'
-import { RiHomeOfficeFill } from '@react-icons/all-files/ri/RiHomeOfficeFill'
-import { FaLaptopCode } from '@react-icons/all-files/fa/FaLaptopCode'
+import { FaFutbol, FaLock, FaMapPin, FaSeedling, FaTractor, FaTruckLoading, FaTint, FaLaptopCode } from 'react-icons/fa'
+import { FaTruckArrowRight } from 'react-icons/fa6'
+import { IoMdFootball } from 'react-icons/io'
+import { IoIosFootball } from 'react-icons/io'
+import { IoFootball, IoFootballOutline, IoStorefrontSharp } from 'react-icons/io5'
+import { RiHomeOfficeFill } from 'react-icons/ri'
+import { TbMapPinDown, TbDropletStar } from 'react-icons/tb'
+import { PiPlant, PiFarm } from 'react-icons/pi'
+
 // others?
 import { IconBaseProps, IconType } from 'react-icons'
 interface MapMarkerIconProps extends IconBaseProps {
