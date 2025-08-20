8d7bc55cd10abb1d6435fc8f171a64d2d00220bb
Navid Kalaei <navidkalaei@gmail.com>
2025-08-20 05:36:26 -0700

    feat: add hourly frequency to the subscription frequency

--- components/SubscriptionModal.tsx
+++ components/SubscriptionModal.tsx
@@ -185,6 +185,7 @@ const SubscriptionModal: FC = () => {
             <Radio.Group
               defaultValue="daily"
             >
+              <Radio value="hourly">{t("subscribeToBbox.frequencies.hour")}</Radio>
               <Radio value="daily">{t("subscribeToBbox.frequencies.day")}</Radio>
               <Radio value="weekly">{t("subscribeToBbox.frequencies.week")}</Radio>
               <Radio value="monthly">{t("subscribeToBbox.frequencies.month")}</Radio>
