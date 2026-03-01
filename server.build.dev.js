076906f30a0c7af890c1cd9d19bf2b1b7cd455cb
Navid Kalaei <navidkalaei@gmail.com>
2026-03-01 13:13:47 -0800

    fix(#256): make the entry archive link configurable

--- server.build.dev.js
+++ server.build.dev.js
@@ -5,6 +5,8 @@ dotenv.config()
 
 process.env.NODE_ENV = 'production'
 process.env.NEXT_PUBLIC_BASICS_API = process.env.NEXT_PUBLIC_BASICS_API || 'https://dev.ofdb.io/v0'
+process.env.NEXT_PUBLIC_OFDB_ENTRY_ARCHIVE_URL = process.env.NEXT_PUBLIC_OFDB_ENTRY_ARCHIVE_URL || 'https://dev.ofdb.io/v0/entries'
+process.env.NEXT_PUBLIC_OFDB_EVENT_ARCHIVE_URL = process.env.NEXT_PUBLIC_OFDB_EVENT_ARCHIVE_URL || 'https://dev.ofdb.io/v0/events'
 process.env.NEXT_PUBLIC_SELF_DOMAIN = process.env.NEXT_PUBLIC_SELF_DOMAIN || `https://dev.kartevonmorgen.org`
 process.env.NEXT_PUBLIC_SELF_API = process.env.NEXT_PUBLIC_SELF_API || `${process.env.NEXT_PUBLIC_SELF_DOMAIN}/api/v0`
 process.env.DB_NAME = process.env.DB_NAME || 'kartevonmorgen.development.sqlite'
